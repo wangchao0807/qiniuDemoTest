01edb39208ff66ebb3cda5bf6c81c11f6b740aa3
wangchao0807 <40227698+wangchao0807@users.noreply.github.com>
2018-09-01 11:38:17 +0800

    spring boot上传文件至七牛

---
+++ src/main/java/com/example/qiniuyun/QiniuyunApplication.java
@@ -0,0 +1,12 @@
+package com.example.qiniuyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiniuyunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QiniuyunApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/qiniuyun/QiniuyunApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.qiniuyun;
+
+import com.example.qiniuyun.service.IQiniuUploadFileService;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.util.UUID;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QiniuyunApplicationTests {
+
+}
